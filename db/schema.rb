f05dc8f6f2bd6d2b801a00a9a230d572507a3da8
Auriane de Premilhat <adeprem@vt.edu>
2017-09-29 17:44:37 +0200

    pas eu le temps de finir le versionning

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170929140805) do
+ActiveRecord::Schema.define(version: 20170929151754) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20170929140805) do
     t.string "name"
     t.string "email"
     t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "versions", force: :cascade do |t|
+    t.string "content"
+    t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
