d0d859b83f4fe87b1c3a7229ce7f142610cd3e16
Auriane de Premilhat <adeprem@vt.edu>
2017-09-29 11:53:42 +0200

    done with the first part- created table, filled it, did the CRUD routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  article = Article.new(
+  title: Faker::Lorem.sentence,
+  content:Faker::Lorem.paragraphs(3, true)
+  )
+  article.save!
+end
